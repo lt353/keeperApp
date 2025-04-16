42e7b8253b0c8d0800c6589a030414e7cbdc50e5
Lindsay Trenton <lt353@hawaii.edu>
2025-04-15 15:22:41 -1000

    Project complete. User can add notes and delete notes.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import notes from "../notes.js";
 
 function Note(props) {
+
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>Delete</button>
+      <button onClick={handleClick}>Delete</button>
     </div>
   );
 }
